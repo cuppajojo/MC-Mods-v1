169bdfb5e14e86bffbd2babe13bfeb02a86e3ec0
cuppajojo <drummerman1099@gmail.com>
2025-04-21 16:15:52 -0500

    added bloodborne hunter item

--- src/main/java/net/cuppajojo/tutorialmod/item/ModItems.java
+++ src/main/java/net/cuppajojo/tutorialmod/item/ModItems.java
@@ -1,7 +1,7 @@
 package net.cuppajojo.tutorialmod.item;
 
-import net.cuppajojo.tutorialmod.TutorialMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.cuppajojo.tutorialmod.TutorialMod;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -9,11 +9,11 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-
+    //public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
-    public static final Item CUPPAJOJO_SMILE = registerItem("cuppajojo_smile", new Item(new Item.Settings()));
-    public static final Item CUPPAJOJO_SAD = registerItem("cuppajojo_sad", new Item(new Item.Settings()));
+    public static final Item BB_HUNTER = registerItem("bb_hunter", new Item(new Item.Settings()));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
     }
@@ -24,10 +24,10 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(PINK_GARNET);
             fabricItemGroupEntries.add(RAW_PINK_GARNET);
-            fabricItemGroupEntries.add(CUPPAJOJO_SMILE);
-            fabricItemGroupEntries.add(CUPPAJOJO_SAD);
+
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(BB_HUNTER);
         });
     }
-
-
 }
